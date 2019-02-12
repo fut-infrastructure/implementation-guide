e3d9d3dc47e3db3fc58a7ec73f8697a92d4ac948
Ole Hedegaard <ohe@trifork.com>
2019-02-12 08:25:30 +0100

    Message/Communication fixes

--- resources/structuredefinition/upload.sh
+++ resources/structuredefinition/upload.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+VALUESETS=../valueset/*.xml
 EXTENSIONS=*-ext-*
-PROFILES="dk-tm-correspondance.xml"
+PROFILES="dk-tm-correspondance.xml dk-tm-message.xml"
+for f in $VALUESETS
+do
+  echo "*** Uploading value set $f..."
+  # cat $f                                                                                                                                                                                                                                                                                                               
+  curl -X POST -H "Content-type: application/fhir+xml" -d @$f  http://hapi.fhir.org/baseDstu3/ValueSet/
+done
 for f in $EXTENSIONS
 do
   echo "*** Uploading extension $f..."
