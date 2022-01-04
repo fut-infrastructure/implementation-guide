cc1243cdfd68987fcf7c79371f1d0f9e70602ffd
tyge.folke.nielsen <tyge.folke.nielsen@@systematic.com>
2022-01-04 13:36:44 +0100

    Run sushi before ig-publisher - to avoid hiding offending errors from sushi

--- fsh-content/_genonce.sh
+++ fsh-content/_genonce.sh
@@ -14,14 +14,16 @@ fi
 
 echo "$txoption"
 
+sushi .
+
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
-	java -jar $publisher -ig . $txoption $*
+	java -jar $publisher -no-sushi -ig . $txoption $*
 
 else
 	publisher=../$publisher_jar
 	if test -f "$publisher"; then
-		java -jar $publisher -ig . $txoption $*
+		java -jar $publisher -no-sushi -ig . $txoption $*
 	else
 		echo IG Publisher NOT FOUND in input-cache or parent folder.  Please run _updatePublisher.  Aborting...
 	fi
