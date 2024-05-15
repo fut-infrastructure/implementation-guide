f0566b848d466e1e768e377490e8164398bc477e
tyfni <tyge.folke.nielsen@systematic.com>
2024-05-15 14:12:59 +0200

    try search engine selector

--- input/pagecontent/search.md
+++ input/pagecontent/search.md
@@ -1,6 +1,11 @@
 <script src="sitesearch.js"></script>
-<form action="https://www.google.com/search" method="get" onSubmit="Gsitesearch(this)">
-<input name="q" type="hidden" />
-<input name="qfront" type="text" style="width: 180px" /> <input type="submit" value="Search" />
+<form id="searchForm" action="javascript:void(0);" onsubmit="sitesearch(this);">
+    <select id="searchEngine">
+        <option value="google">Google</option>
+        <option value="bing">Bing</option>
+        <!-- Add more options for other search engines if needed -->
+    </select>
+    <input type="text" name="qfront" style="width: 180px" />
+    <input type="submit" value="Search" />
 </form>
 <br>
--- input/pagecontent/sitesearch.js
+++ input/pagecontent/sitesearch.js
@@ -1,9 +1,25 @@
-var version="{{site.data.fhir.igVer}}";
-if (version != "latest") {
-    version = "v" + version;
+function sitesearch(curobj) {
+    var searchEngine = document.getElementById("searchEngine").value;
+    var searchQuery = curobj.qfront.value;
+    var searchUrl = "";
+
+    switch (searchEngine) {
+        case "google":
+            searchUrl = "https://www.google.com/search";
+            break;
+        case "bing":
+            searchUrl = "https://www.bing.com/search";
+            break;
+        // Add cases for other search engines if needed
+
+        default:
+            searchUrl = "https://www.google.com/search"; // Default to Google
+            break;
+    }
+
+    // Construct the search query URL based on the selected search engine
+    searchUrl += "?q=site:" + domainroot + " " + searchQuery;
+
+    // Redirect to the constructed search query URL
+    window.location.href = searchUrl;
 }
-var domainroot="https://docs.ehealth.sundhed.dk/" + version + "/ig/"
-
-function Gsitesearch(curobj)
-
-{ curobj.q.value="site:"+domainroot+" "+curobj.qfront.value }
