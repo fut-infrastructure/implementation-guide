365d4c04e2a392639d9c9b1bf82f96733752e525
matgs <mathias.glavind.schmidt@systematic.com>
2025-09-25 10:48:31 +0200

    FUT1-20316 Attempt to lock IG publisher version

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pubsource=https://github.com/HL7/fhir-ig-publisher/releases/tag/2.0.16/download/
+pubsource=https://github.com/HL7/fhir-ig-publisher/releases/latest/download/
 publisher_jar=publisher.jar
 dlurl=$pubsource$publisher_jar
 
