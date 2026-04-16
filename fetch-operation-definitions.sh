9c445168aada391f2c93c07c55b4b3f7dd1a19eb
tyfni <tyge.folke.nielsen@systematic.com>
2026-04-16 08:01:59 +0200

    FUT1-23057 handle token timeout by login more frequent

--- fetch-operation-definitions.sh
+++ fetch-operation-definitions.sh
@@ -45,7 +45,7 @@ function fetch_operation_definitions {
 
 #	for service in $services; do
 	for service in "${SERVICES[@]}"; do
-
+    login
 	  echo "Handling ${service} service"
 
 		file="$OUTPUT_DIR/${service}.xml"
@@ -69,7 +69,7 @@ function fetch_operation_definitions {
         curl_exit=$?
         if [ "$curl_exit" -ne 0 ] || [ "$http_code" -ne 200 ]; then
           echo "ERROR: Fetching ${operation} terminated with curl exit-code: $curl_exit; http-code: $http_code"
-          exit 1
+          continue
         fi
 
 				id=$(jq -r '.id' "$tmp")
